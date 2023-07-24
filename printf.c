f9baae82c212551c90eb273aff5f3514cd36f75a
Philip <nukem8475@gmail.com>
2023-07-24 03:44:57 -0700

    m

---
+++ printf.c
@@ -0,0 +1,57 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - Prints formatted output to stdout.
+ * @format: The format string containing directives.
+ *
+ * Return: The number of characters printed (excluding null byte).
+ */
+int _printf(const char *format, ...)
+{
+    int count = 0;
+    va_list args;
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                int c = va_arg(args, int);
+                putchar(c);
+                count++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                while (*str)
+                {
+                    putchar(*str);
+                    count++;
+                    str++;
+                }
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                count++;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
+
