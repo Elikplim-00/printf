bf509557e7ae5e7c02cb399f67aca2285ac7e74b
Philip <nukem8475@gmail.com>
2023-07-24 03:51:47 -0700

    m

--- printf.c
+++ printf.c
@@ -1,57 +1,54 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
  * _printf - Prints formatted output to stdout.
  * @format: The format string containing directives.
- *
  * Return: The number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
+	int count = 0;
+	va_list args;
 
-    va_start(args, format);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                while (*str)
-                {
-                    putchar(*str);
-                    count++;
-                    str++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+				putchar(c);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
 
-    va_end(args);
+				while (*str)
+				{
+					putchar(*str);
+					count++;
+					str++;
+				}
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
 
-    return count;
+	va_end(args);
+	return (count);
 }
-
