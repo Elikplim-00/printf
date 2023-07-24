d0a4fbfb9e34633fb9c3278aa251cec56da84813
Philip <nukem8475@gmail.com>
2023-07-24 04:14:31 -0700

    m

--- main.h
+++ main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 int _printf(const char *format, ...);
-
+int _print_str(char *str);
+int _print_char(char c);
 #endif /* MAIN_H */
 
--- printf.c
+++ printf.c
@@ -2,8 +2,40 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _print_char - Prints a character to stdout.
+ * @c: The character to print.
+ *
+ * Return: The number of characters printed (1).
+ */
+int _print_char(char c)
+{
+	putchar(c);
+	return (1);
+}
+
+/**
+ * _print_str - Prints a string to stdout.
+ * @str: The string to print.
+ *
+ * Return: The number of characters printed (excluding null byte).
+ */
+int _print_str(char *str)
+{
+	int count = 0;
+
+	while (*str)
+	{
+		putchar(*str);
+		count++;
+		str++;
+	}
+
+	return (count);
+}
+/**
  * _printf - Prints formatted output to stdout.
  * @format: The format string containing directives.
+ *
  * Return: The number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
@@ -12,43 +44,25 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
-
-				putchar(c);
-				count++;
-			}
+				count += _print_char(va_arg(args, int));
 			else if (*format == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				while (*str)
-				{
-					putchar(*str);
-					count++;
-					str++;
-				}
-			}
+				count += _print_str(va_arg(args, char *));
 			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
+				count += _print_char('%');
 		}
 		else
-		{
-			putchar(*format);
-			count++;
-		}
+			count += _print_char(*format);
 		format++;
 	}
 
 	va_end(args);
+
 	return (count);
 }
